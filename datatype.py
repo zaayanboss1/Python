a08536540b96636ed958957fd529d70436610aa9
zaayanboss1 <zaayanibnmobarak@gmail.com>
2025-06-23 21:43:27 +0600

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,8 @@
+a=5
+print("type of a is",type(a))
+b=6.5
+print("type of b is",type(b))
+c="carnival"
+print("type of c is",type(c))
+d=True
+print("type of d is",type(d))
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import keyword
-print("Python keywords are")
-print(keyword.kwlist)
+name="Benzema"
+age=15
+weight=48.9
+is_student=True
+print("your name is",name)
+print("your age is",age)
+print("your weight is",weight)
+print("you are student",is_student)
+print("data type of name is",type (name))
+print("data type of age is",type(age))
+print("data type of weight is",type(weight))
+weight=int(weight)
+print("your new weight is",weight)
+age=str(age)
+print("your new age is",age)
