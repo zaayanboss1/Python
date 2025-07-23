93013de1491979362945b1ba3ccc30f0ff051494
zaayanboss1 <zaayanibnmobarak@gmail.com>
2025-07-23 21:26:04 +0600

    Add files via upload

---
+++ reverse order.py
@@ -0,0 +1,3 @@
+n=int(input("enter the last number"))
+for i in range(n,0,-1):
+    print(i)
---
+++ reversed string.py
@@ -0,0 +1,6 @@
+string1=input("enter any word")
+string2=" "
+for i in string1:
+    string2=i+string1
+print("the original string is",string1)
+print("the reversed string is",string2)
