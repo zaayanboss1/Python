1a2bb5303cd53b0dd2bf744e165dc973a375fffd
zaayanboss1 <zaayanibnmobarak@gmail.com>
2025-10-13 21:41:23 +0600

    Add files via upload

---
+++ Upper case.py
@@ -0,0 +1,12 @@
+class IOString():
+    def _init_(self):
+        self.str1=""
+    def get_String(self):
+        self.str1=input("Enter String: ")
+    def print_String(self):
+        print("Result is :",self.str1.upper)
+str1=IOString()
+str1.get_String()
+str1.print_String()
+
+
