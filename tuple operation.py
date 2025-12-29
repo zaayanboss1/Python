fa68dee4d8fd89a28a0bcba378be72ce1a8e2d57
zaayanboss1 <zaayanibnmobarak@gmail.com>
2025-12-29 21:43:07 +0600

    Add files via upload

---
+++ tuple operation.py
@@ -0,0 +1,26 @@
+my_tuple = ()
+print(my_tuple)
+
+my_tuple=(1,2,3)
+print(my_tuple)
+
+my_tuple=(1,"Hello",3.4)
+print(my_tuple)
+
+my_tuple=("mouse",[8,4,6],(1,2,3))
+print(my_tuple)
+
+my_tuple=('p','e','r','m','i','t')
+print(my_tuple[0])
+print(my_tuple[5])
+
+n_tuple = ("mouse",[8,4,6],(1,2,3))
+
+print(n_tuple[0][3])
+print(n_tuple[1][1])
+
+print("Sliced :",my_tuple[1:4])
+
+for letter in(my_tuple):
+    print("Hello", letter)
+
