85b061cef28e1d3521e03c84d88a486b32868e19
zaayanboss1 <zaayanibnmobarak@gmail.com>
2026-01-08 12:28:46 +0600

    Add files via upload

---
+++ inheratence.py
@@ -0,0 +1,19 @@
+class Person(object):
+    def __init__(self,name,idnumber):
+        self.name=name
+        self.idnumber = idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+class Employee(Person):
+    def __init__(self,name,idnumber,salary,post):
+        self.salary = salary
+        self.post=post
+
+class
+        Person.__init__(self,name,idnumber)
+
+
+a = Employee('Penguin',202110401, 15000,"Intern")
+a.display()
