8c371417f4aa5bb72197c3b9295f7977c8ea1de3
zaayanboss1 <zaayanibnmobarak@gmail.com>
2026-01-19 21:31:22 +0600

    Add files via upload

---
+++ polymorphysom.py
@@ -0,0 +1,28 @@
+class cat:
+    def __init__(self,name,age):
+        self.name= name
+        self.age = age
+
+    def info(self):
+        print(f"I am a cat. My name is {self.name}. I am {self.age} years old")
+
+    def make_sound(self):
+        print("Meow")
+
+class Dog:
+    def __init__(self,name,age):
+        self.name= name
+        self.age = age
+    
+    def info(self):
+        print(f"I am a dog. My name is {self.name}.I am {self.age} years old")
+
+    def make_sound(self):
+        print("Bark")
+
+cat1= cat("Dodo", 2.5)
+dog1=Dog("Tyson",8)
+
+for animal in (cat1,dog):
+    animal.make_sound()
+    animal.info()
