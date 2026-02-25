ae9ce9de6498fec784a495218869de868d75fac4
zaayanboss1 <zaayanibnmobarak@gmail.com>
2026-02-25 21:43:36 +0600

    Add files via upload

---
+++ factorial using recurtion.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if (n==1 or n==0):
+        return 1
+    else:
+        return(n*factorial(n-1))
+    
+num=5
+print("number:",num)
+print("factorial:",factorial(num))
+
+number:5
+factorial:120
