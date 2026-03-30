063d5ed8df97741d71a32c571a626baeb4b05cb3
zaayanboss1 <zaayanibnmobarak@gmail.com>
2026-03-30 21:42:42 +0600

    Add files via upload

---
+++ power of 2.py
@@ -0,0 +1,12 @@
+def power2(number):
+    if(number == 0):
+        return 0
+    if((number &(~(number -1 )))== number):
+        return 1
+    return 0
+number = int(input("Enter the number: "))
+if(power2(number)):
+    print("\nThe number is power 0f 2")
+
+else:
+    print("\nThe number is not power of 2")
