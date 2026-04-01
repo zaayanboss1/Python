255e22c81b636271dde82dec2a3f4f73c829da18
zaayanboss1 <zaayanibnmobarak@gmail.com>
2026-04-01 21:44:07 +0600

    Add files via upload

---
+++ swapping using xor.py
@@ -0,0 +1,15 @@
+def swap1(a,b):
+
+    a = a ^ b
+    b= a ^ b
+    a= a^ b
+    print("After Swapping : a =",a,"b=",b)
+
+def swap2(a,b):
+    a = (a&b)+(a|b)
+    b = a+(~b)+1
+    a = a+(~b)+1
+    print("after Swapping: a =",a,"b=",b)
+
+swap1(1,2)
+swap2(2,1)
